e76e4ff05c7aef8eb59ad54abbac2d78c9fe053f
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-22 17:14:29 -0400

    making example of what whole post looks like

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,57 @@ function App() {
   return (
     <div className="App">
       <BoxedContent />
-
-      <BoxedContent />
-
-      <BoxedContent />
+      <BoxedContentWholePost />
     </div>
   );
 }
 
+//base area
 function BoxedContent() {
   return (
     <div className="post-box">
       <p>Username</p>
       <h2>Header</h2>
       <div className="post-content">
-        <p>text content</p>
+        <p>text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content </p>
         </div>
         <div className="bottom-content">
           <table>
+            <thead></thead>
+            <tbody>
+              <tr>
+                <th>like</th>
+                <th>comment</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    </div>
+  )
+}
+
+function BoxedContentWholePost() {
+  return (
+    <div className="whole-post-box">
+      <p>Username</p>
+      <h2>Whole Post Header</h2>
+      <div className="whole-post-content">
+        <p>text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content text content </p>
+      </div>
+      <div className="whole-post-bottom-content">
+        <table>
+          <thead></thead>
+          <tbody>
             <tr>
               <th>like</th>
               <th>comment</th>
             </tr>
-          </table>
-        </div>
+          </tbody>
+        </table>
+      </div>
+      <div className="whole-post-comment-box">
+        <h2>Comment</h2>
+      </div>
     </div>
   )
 }
