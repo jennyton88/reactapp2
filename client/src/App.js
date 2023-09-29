3035ca6b3d28f1bf92ba2e688d39f3656ede4d6f
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-29 17:40:39 -0400

    add buttons, controls, not work properly

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,21 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  // const arr = [];
+  //const [testDiv, setTestDiv] = useState(arr);
+
+  // function makeDivs() {
+  //   arr.push(<BoxedContent />);
+  //   const arrDivs = arr.map(postDiv => <BoxedContent />);
+  //   return <div>{arrDivs}</div>;
+  // }
   return (
     <div className="App">
-      <AddPost />
+      {/* <button onClick={() => makeDivs()}>Add a boxed Content</button> */}
+      {/* <AddPost /> */}
+      <BoxedContent />
+      <BoxedContent />
+      <BoxedContent />
       <BoxedContent />
       <BoxedContentWholePost />
     </div>
@@ -13,6 +25,8 @@ function App() {
 
 //base area
 function BoxedContent() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="post-box">
       <p>Username</p>
@@ -25,7 +39,7 @@ function BoxedContent() {
             <thead></thead>
             <tbody>
               <tr>
-              <th><button>like</button></th>
+              <th><button onClick={() => setCount(1)}>like {count}</button></th>
               <th><button>comment</button></th>
               </tr>
             </tbody>
@@ -35,19 +49,30 @@ function BoxedContent() {
   )
 }
 
-function AddPost() {
-  return(
-    <div>
-      <button>Add a post</button>
-    </div>
-  )
-}
+// function AddPost() {
+//   return(
+//     <div>
+//       <button>Add a post</button>
+//     </div>
+//   )
+// }
 
 
 function BoxedContentWholePost() {
   const [count, setCount] = useState(0); // right now not accounting for user like a post once
   const [saveComment, setComment] = useState("");
   const [textContent, setTextContent] = useState("");
+  // const [numOfDivs, setNumOfDivs] = useState(0);
+  // const [divs, setDivs] = useState(null);
+
+  function createNewComment(textContent) {
+    setComment(textContent);
+    // setNumOfDivs(numOfDivs + 1);
+  }
+
+  // function createCommentDivs() {
+
+  // }
 
   return (
     <div className="whole-post-box">
@@ -69,12 +94,20 @@ function BoxedContentWholePost() {
       </div>
       <div className="whole-post-comment-box">
         <textarea className="whole-post-comment-textarea" value={textContent} onChange={e => setTextContent(e.target.value)}></textarea>
-        <button onClick={() => setComment(textContent)}>Comment</button>
+        <button onClick={() => createNewComment(textContent)}>Comment</button>
         <p>{saveComment}</p>
       </div>
     </div>
   )
 }
+
+// function AddCommentDiv({textContent}) {
+//   return (
+//     <div>
+//       <p>{textContent}</p>
+//     </div>
+//   )
+// }
 
 
 // function AddPost() {
