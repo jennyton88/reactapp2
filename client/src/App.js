b56ae83c30923b43946ad3454c470cbfcd1d7037
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-02 16:42:14 -0400

    attempting to make custom posts

--- client/src/App.js
+++ client/src/App.js
@@ -12,21 +12,59 @@ import WholeUserPost from "./components/WholeUserPost";
 
 
 function App() {
-
   const [backendData, setBackendData] = useState([{}]);
+  const [parsedData, setParsedData] = useState([{}]);
+
+  const [username, setUsername] = useState("");
+  const [header, setHeader] = useState("");
+  const [userText, setUserText] = useState("");
+  const [userLikes, setUserLikes] = useState(0);
 
   useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
+    fetch("/posts").then(
+      response => response.json(),
     ).then(
       data => {
+        console.log("original", data);
         setBackendData(data);
       }
     )
   }, []);
 
+  function updateData() {
+    setUserLikes(1);
+
+    const test = {
+        "key": username + header,
+        "username": username,
+        "header": header,
+        "postText": userText, 
+        "likes": 1000
+    }
+
+    fetch("/posts", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(test)
+    }).then(
+      response => response.json(),
+    ).then(
+      data => {
+        console.log("new", data);
+        setBackendData(data);
+      }
+    )
+  }
+
   return (
     <div className="App">
+      <textarea className="username" value={username} onChange={e => setUsername(e.target.value)}></textarea>
+      <br></br>
+      <textarea className="header" value={header} onChange={e => setHeader(e.target.value)}></textarea>
+      <br></br>
+      <textarea className="userText" value={userText} onChange={e => setUserText(e.target.value)}></textarea>
+      <br></br>
+      <button onClick={() => updateData()}>Add posts</button>
 
       {(typeof backendData.posts === "undefined") ? (
         <p>Loading...</p>
@@ -34,7 +72,7 @@ function App() {
 
         backendData.posts.map((posted) => <UserPost key={posted.key} username={posted.username} header={posted.header} postText={posted.postText} likes={posted.likes} />)
       )}
-      <WholeUserPost />
+      {/* <WholeUserPost /> */}
     </div>
   );
 }
