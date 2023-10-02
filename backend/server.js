aea7f4f0cbec3050d6058d7f276e3ec6a837b9d0
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-02 17:21:32 -0400

    add json-server locally

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express()
 
+const jsonServer = require("json-server");
+
+
+// app.use("/api", jsonServer.router("db.json"));
+// app.listen(3000);
 app.use(express.json());
 
 app.get("/posts", (req, res) => {
@@ -24,30 +29,29 @@ app.get("/posts", (req, res) => {
     });
 });
 
-app.post("/posts", (req, res) => {
-    console.log(req.body);
-    let test = {
-        "posts": [
-            {
-                "key": "Person-What-is-the-future?",
-                "username": "Person",
-                "header": "What is the future?",
-                "postText": "AI", 
-                "likes": 0
-            },
-            {
-                "key": "Person2-Dogs-are-good-pets", 
-                "username": "Person2", 
-                "header": "Dogs are good pets", 
-                "postText": "Source? me", 
-                "likes": 3
-            }
-        ]
-    };
+let test = {
+    "posts": [
+        {
+            "key": "Person-What-is-the-future?",
+            "username": "Person",
+            "header": "What is the future?",
+            "postText": "AI", 
+            "likes": 0
+        },
+        {
+            "key": "Person2-Dogs-are-good-pets", 
+            "username": "Person2", 
+            "header": "Dogs are good pets", 
+            "postText": "Source? me", 
+            "likes": 3
+        }
+    ]
+};
 
+app.post("/posts", (req, res) => {
+    
     test["posts"].push(req.body);
     //console.log("here", test["posts"]);
-
     
     res.json(test);
 })
