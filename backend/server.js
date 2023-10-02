c5a7ce0b6c6ae76e80158c13023c0d6a7503f516
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-01 21:36:19 -0400

    changed data to reflect posts data

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,30 @@ const express = require("express");
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]});
+    // res.json({
+    //     "users": ["userOne", "userTwo", "userThree"],
+    //     "work": "hello"
+    // });
+
+    res.json({
+        "posts": [
+            {
+                "key": "Person-What-is-the-future?",
+                "username": "Person",
+                "header": "What is the future?",
+                "postText": "AI", 
+                "likes": 0
+            },
+            {
+                "key": "Person2-Dogs-are-good-pets", 
+                "username": "Person2", 
+                "header": "Dogs are good pets", 
+                "postText": "Source? me", 
+                "likes": 3
+            }
+        ]
+    });
+
 });
 
 app.listen(5000, () => {
