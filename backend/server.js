713d09ed29553e00ccfcf67cf1fffca579de6957
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 17:10:58 -0400

    remove connection to json server

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const app = express()
 
-const jsonServer = require("json-server");
+// const jsonServer = require("json-server");
+// app.use("/posts", jsonServer.router("db.json"));
+// app.listen(3001);
 
-// app.use("/api", jsonServer.router("db.json"));
-// app.listen(3000);
+
+
 app.use(express.json());
 
 app.get("/posts", (req, res) => {
     console.log(req.body);
-    // res.json({
-    //     "posts": [
-    //         {
-    //             "key": "Person-What-is-the-future?",
-    //             "username": "Person",
-    //             "header": "What is the future?",
-    //             "postText": "AI", 
-    //             "likes": 0
-    //         },
-    //         {
-    //             "key": "Person2-Dogs-are-good-pets", 
-    //             "username": "Person2", 
-    //             "header": "Dogs are good pets", 
-    //             "postText": "Source? me", 
-    //             "likes": 3
-    //         }
-    //     ]
-    // });
+    res.json({
+        "posts": [
+            {
+                "key": "Person-What-is-the-future?",
+                "username": "Person",
+                "header": "What is the future?",
+                "postText": "AI", 
+                "likes": 0
+            },
+            {
+                "key": "Person2-Dogs-are-good-pets", 
+                "username": "Person2", 
+                "header": "Dogs are good pets", 
+                "postText": "Source? me", 
+                "likes": 3
+            }
+        ]
+    });
 });
 
 let test = {
