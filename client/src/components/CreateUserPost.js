6db9029983b5516913104c016b385af33cf6e078
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 19:44:41 -0400

    working on create user post

--- client/src/components/CreateUserPost.js
+++ client/src/components/CreateUserPost.js
@@ -1,23 +1,49 @@
+import React, {useState} from 'react';
 
 export default function CreateUserPost() {
     
+    const [username, setUsername] = useState("");
+    const [header, setHeader] = useState("");
+    const [userText, setUserText] = useState("");
+    const [userLikes, setUserLikes] = useState(0);
+    
+    function handlePost() {
+        const createdPost = {
+            "key": username + header,
+            "username": username,
+            "header": header,
+            "postText": userText, 
+            "likes": 0
+        }
+    
+        fetch("/posts", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(createdPost)
+        }).then(
+          response => response.json()
+        )
+        // ).then(
+        // //   data => {
+        // //     console.log("new", data);
+        // //     setBackendData(data);
+        // //   }
+        // )
+    }
+    
     return (
         <div className="post-box">
-            <p>{username}</p>
-            <h2>{header}</h2>
+            <label>Username:</label> <br />
+            <textarea className="username" value={username} onChange={e => setUsername(e.target.value)}></textarea>
+            <br />
+            <label>Header:</label> <br />
+            <textarea className="header" value={header} onChange={e => setHeader(e.target.value)}></textarea>
             <div className="post-content">
-                <p>{postText}</p>
+                <label>Text:</label> <br />
+                <textarea className="userText" value={userText} onChange={e => setUserText(e.target.value)}></textarea>
             </div>
             <div className="bottom-content">
-                <table>
-                    <thead></thead>
-                    <tbody>
-                    <tr>
-                        <th><button onClick={() => changeLikes()}>like {likes}</button></th>
-                        <th><button>comment</button></th>
-                    </tr>
-                    </tbody>
-                </table>
+                    <button onClick={() => handlePost()}>Post</button>
             </div>
         </div>
     )
