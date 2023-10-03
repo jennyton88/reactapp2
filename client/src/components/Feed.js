c8ce17409969fddab47a22d6819668b5c42a06ca
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 17:11:35 -0400

    added feed component

---
+++ client/src/components/Feed.js
@@ -0,0 +1,14 @@
+import UserPost from "./UserPost";
+
+export default function Feed() {
+    return (
+        <div>
+            {/* {(typeof backendData.posts === "undefined") ? (
+        <p>Loading...</p>
+      ): (
+
+        backendData.posts.map((posted) => <UserPost key={posted.key} username={posted.username} header={posted.header} postText={posted.postText} likes={posted.likes} />)
+      )} */}
+        </div>
+    )
+}
