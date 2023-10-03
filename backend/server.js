af9b8fb5feeb81acd53ae23ff39f3b2772c3f125
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-02 22:05:38 -0400

    moving tests to json server

--- backend/server.js
+++ backend/server.js
@@ -3,30 +3,30 @@ const app = express()
 
 const jsonServer = require("json-server");
 
-
 // app.use("/api", jsonServer.router("db.json"));
 // app.listen(3000);
 app.use(express.json());
 
 app.get("/posts", (req, res) => {
-    res.json({
-        "posts": [
-            {
-                "key": "Person-What-is-the-future?",
-                "username": "Person",
-                "header": "What is the future?",
-                "postText": "AI", 
-                "likes": 0
-            },
-            {
-                "key": "Person2-Dogs-are-good-pets", 
-                "username": "Person2", 
-                "header": "Dogs are good pets", 
-                "postText": "Source? me", 
-                "likes": 3
-            }
-        ]
-    });
+    console.log(req.body);
+    // res.json({
+    //     "posts": [
+    //         {
+    //             "key": "Person-What-is-the-future?",
+    //             "username": "Person",
+    //             "header": "What is the future?",
+    //             "postText": "AI", 
+    //             "likes": 0
+    //         },
+    //         {
+    //             "key": "Person2-Dogs-are-good-pets", 
+    //             "username": "Person2", 
+    //             "header": "Dogs are good pets", 
+    //             "postText": "Source? me", 
+    //             "likes": 3
+    //         }
+    //     ]
+    // });
 });
 
 let test = {
