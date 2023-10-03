d62aa9e0329bcb568f767fa57990255cebade66f
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 17:11:43 -0400

    added topbar

---
+++ client/src/components/TopBar.js
@@ -0,0 +1,15 @@
+
+
+export default function TopBar() {
+
+
+    return (
+        <nav className="top-bar">
+            <h1>Top Bar</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/post-on-feed">Create post</a>
+            </div>
+        </nav>
+    )
+}
