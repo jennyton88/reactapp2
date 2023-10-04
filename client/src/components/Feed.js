cac6d1ba91b02117d7fe29de89b298712fd5eba3
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 20:53:39 -0400

    trying to organize

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -2,14 +2,14 @@ import React, {useState, useEffect} from 'react';
 import UserPost from "./UserPost";
 
 export default function Feed({backendData}) {
-
   return (
-    <div>
-      
-      {(typeof backendData.posts === "undefined") ? (
-        <p>Loading...</p>
-      ): (
-        backendData.posts.map((posted) => <UserPost key={posted.key} username={posted.username} header={posted.header} postText={posted.postText} likes={posted.likes} />)
+    <div>      
+      {(typeof backendData.posts === "undefined") ? (<p>Loading...</p>): (
+        backendData.posts.map((post) => <UserPost key={post.key} 
+                                              username={post.username} 
+                                                header={post.header} 
+                                                  postText={post.postText} 
+                                                    likes={post.likes} />)
       )}
     </div>
   )
