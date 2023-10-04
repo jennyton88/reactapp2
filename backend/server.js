8e2549184dc4c515d908884e3fd56a1c8880368a
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 23:36:30 -0400

    added get for id

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use(express.json());
 
+// remember to reset server to update database
+
 let test = {
     "posts": [
         {
@@ -18,6 +20,13 @@ let test = {
             "header": "Dogs are good pets", 
             "postText": "Source? me", 
             "likes": 3
+        },
+        {
+            "key": "Person3-Test", 
+            "username": "Person3", 
+            "header": "Test", 
+            "postText": "123", 
+            "likes": 20
         }
     ]
 };
@@ -46,12 +55,37 @@ app.get("/posts", (req, res) => {
     // });
 });
 
+app.get("/posts/:key", (req, res) => {
+    // res.send(req.params);
+    // console.log(req.params);
+    // for (post of test.posts) {
+    //     console.log(posts);
+    //     if (post.key == req.params) {
+    //         console.log("yea");
+    //         //res.json(post);
+    //     }
+    // }
+
+    // console.log(test.posts);
+    for (const post of test.posts) {
+        console.log(post.key == req.params.key);
+        console.log(post.key, "   ", req.params.key);
+        if (post.key === req.params.key) {
+            res.send(post);
+            console.log(post);
+            break;
+        }
+        //console.log(req.params.key);
+    }
+    //res.json(test.posts[key] ===);
+})
+
 app.post("/posts", (req, res) => {
     
-    test["posts"].push(req.body);
+    //test["posts"].push(req.body);
     //console.log("here", test["posts"]);
     
-    res.json(test);
+    //res.json(test);
 })
 
 app.listen(5000, () => {
