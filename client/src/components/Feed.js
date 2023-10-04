6ca6ffb319a2213bdffe04d77f62ea2198310f52
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 20:12:16 -0400

    adding fetch for feed

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,14 +1,26 @@
+import React, {useState, useEffect} from 'react';
 import UserPost from "./UserPost";
 
 export default function Feed() {
-    return (
-        <div>
-            {/* {(typeof backendData.posts === "undefined") ? (
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/posts").then(
+      response => response.json())
+      .then(data => {
+        console.log("original", data);
+        setBackendData(data);
+      }
+    )
+  }, []);
+
+  return (
+    <div>
+      {(typeof backendData.posts === "undefined") ? (
         <p>Loading...</p>
       ): (
-
         backendData.posts.map((posted) => <UserPost key={posted.key} username={posted.username} header={posted.header} postText={posted.postText} likes={posted.likes} />)
-      )} */}
-        </div>
-    )
+      )}
+    </div>
+  )
 }
