5028da13884d7ad8d1eecbf654fb3cd17608e5c4
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 20:45:06 -0400

    rename home to feed

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,13 +1,11 @@
 
 
 export default function TopBar() {
-
-
     return (
         <nav className="top-bar">
             <h1>Top Bar</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <a href="/">Feed</a>
                 <a href="/post-on-feed">Create post</a>
             </div>
         </nav>
