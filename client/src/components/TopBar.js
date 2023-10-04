e8259e70facd3a62072daa7952b2154046358151
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-10-03 22:20:15 -0400

    changed link names, imported link changed text

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,12 +1,12 @@
-
+import {Link} from 'react-router-dom';
 
 export default function TopBar() {
     return (
         <nav className="top-bar">
-            <h1>Top Bar</h1>
-            <div className="links">
-                <a href="/">Feed</a>
-                <a href="/post-on-feed">Create post</a>
+            <h1>Welcome to the feed!</h1>
+            <div className="links-around-feed">
+                <Link to="/">Feed</Link>
+                <Link to="/create-post">Create post</Link>
             </div>
         </nav>
     )
